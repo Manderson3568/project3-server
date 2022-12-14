8957bbf9569ba89065ccf9068e44ecbfa232a689
Michael Anderson <manderson3568@outlook.com.au>
2022-12-14 17:48:53 +1100

    nodemon setup

--- api/controllers/studentLessonController.js
+++ api/controllers/studentLessonController.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const StudentLesson = mongoose.model("StudentLesson");
 
 exports.readStudentLessons = (req, res) => {
+  // console.log("back end");
   StudentLesson.find({}, (err, studentlessons) => {
     if (err) res.send(err);
     res.json(studentlessons);
@@ -10,8 +11,9 @@ exports.readStudentLessons = (req, res) => {
 };
 
 exports.readStudentLesson = (req, res) => {
-  StudentLesson.find({}, (err, studentlesson) => {
-    if (err) res.send(err);
-    res.json(studentlesson);
-  });
+  console.log(req.query);
+  // StudentLesson.find({}, (err, studentlesson) => {
+  //   if (err) res.send(err);
+  //   res.json(studentlesson);
+  // });
 };
