a514aeff16c103454a575ebe6fc886c1346e3423
Michael Anderson <manderson3568@outlook.com.au>
2022-12-14 15:18:20 +1100

    start changed to node

--- api/controllers/studentLessonController.js
+++ api/controllers/studentLessonController.js
@@ -2,9 +2,16 @@ const { response } = require("express");
 const mongoose = require("mongoose");
 const StudentLesson = mongoose.model("StudentLesson");
 
-exports.readStudentLesson = (req, res) => {
+exports.readStudentLessons = (req, res) => {
   StudentLesson.find({}, (err, studentlessons) => {
     if (err) res.send(err);
     res.json(studentlessons);
+  });
+};
+
+exports.readStudentLesson = (req, res) => {
+  StudentLesson.find({}, (err, studentlesson) => {
+    if (err) res.send(err);
+    res.json(studentlesson);
   });
 };
--- api/routes/studentLessonRoutes.js
+++ api/routes/studentLessonRoutes.js
@@ -2,4 +2,5 @@ const studentLessonBuilder = require("../controllers/studentLessonController");
 
 module.exports = (app) => {
   app.route("/studentlessons").get(studentLessonBuilder.readStudentLesson);
+  app.route("/studentlesson").get(studentLessonBuilder.readStudentLesson);
 };
