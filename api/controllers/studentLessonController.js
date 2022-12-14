8c60dc9aedf0f9918cfa8556d03d1e6421f237a9
Michael Anderson <manderson3568@outlook.com.au>
2022-12-14 21:17:50 +1100

    stdent/lesson WIP

--- api/controllers/studentLessonController.js
+++ api/controllers/studentLessonController.js
@@ -12,8 +12,33 @@ exports.readStudentLessons = (req, res) => {
 
 exports.readStudentLesson = (req, res) => {
   console.log(req.query);
-  // StudentLesson.find({}, (err, studentlesson) => {
-  //   if (err) res.send(err);
-  //   res.json(studentlesson);
-  // });
+  StudentLesson.find(
+    {
+      studentId: req.query.student,
+      lessonId: req.query.lesson,
+    },
+    (err, studentlesson) => {
+      if (err) res.send(err);
+      return res.json(studentlesson);
+    }
+  );
+};
+
+exports.createStudentLesson = (req, res) => {
+  console.log(req.body);
+  const newStudentLesson = new StudentLesson({
+    lessonID: req.body.lesson,
+    studentId: req.body.student,
+    position: 0,
+  });
+  console.log(newStudentLesson);
+  newStudentLesson.save((err, studentLesson) => {
+    if (err) {
+      return res.status(400).json({
+        title: "error",
+        error: "email in use",
+      });
+    }
+    return studentLesson;
+  });
 };
--- api/routes/studentLessonRoutes.js
+++ api/routes/studentLessonRoutes.js
@@ -1,6 +1,9 @@
 const studentLessonBuilder = require("../controllers/studentLessonController");
 
 module.exports = (app) => {
-  app.route("/studentlessons").get(studentLessonBuilder.readStudentLessons);
+  app
+    .route("/studentlessons")
+    .get(studentLessonBuilder.readStudentLessons)
+    .post(studentLessonBuilder.createStudentLesson);
   app.route("/studentlesson").get(studentLessonBuilder.readStudentLesson);
 };
